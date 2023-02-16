5c3736c4d9eac8bb434126c9c5df52161975a50b
xsandersky <xsandersky@gmail.com>
2023-02-16 22:55:45 +0200

    fix task7

--- hw6/task7.py
+++ hw6/task7.py
@@ -1,22 +1,9 @@
-def fibo(x):
-    global x1
-    global x2
-    global fibo_list
-    for i in range(x-2): 
-        count = x1 + x2
-        fibo_list.append(count)
-        x1 = x2
-        x2 = count
-    return count
+def fibonacci(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return (fibonacci(n - 1) + fibonacci(n - 2))
 
-
-def main():
-    results = fibo(20)
-    print(results)
-    print(fibo_list)
-
-
-x1, x2 = 0, 1
-fibo_list = [0, 1]
-
-main()
+print(fibonacci(20))
