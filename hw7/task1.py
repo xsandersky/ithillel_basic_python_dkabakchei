309975741f2890f92a40ac361056c39e8f03f176
xsandersky <xsandersky@gmail.com>
2023-02-18 17:35:27 +0200

    fix 1_task

--- hw7/task1.py
+++ hw7/task1.py
@@ -1,8 +1,5 @@
 def move_elephant(x1, y1, x2, y2):
-    if (abs(x2 - x1) == 1 and abs(y2 - y1) == 2) or (abs(x2 - x1) == 2 and abs(y2 - y1) == 1):
-        return True
-    else:
-        return False
+    return (abs(x2 - x1) == 1 and abs(y2 - y1) == 2) or (abs(x2 - x1) == 2 and abs(y2 - y1) == 1)
 
 
 def main():
@@ -10,7 +7,7 @@ def main():
     x1, y1 = int(ord(cell_1[0])), int(cell_1[1])
     x2, y2 = int(ord(cell_2[0])), int(cell_2[1])
     
-    if move_elephant(x1, y1, x2, y2) is True:
+    if move_elephant(x1, y1, x2, y2):
         print('Так, кінь потрапить на цю клітину за 1 хід')
     else:
         print('Ні, за один хід це неможливо')
