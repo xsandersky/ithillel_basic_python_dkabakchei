945a9144cb1b2ef8fd9a7bac8169693fa9b72bc0
xsandersky <xsandersky@gmail.com>
2023-02-20 16:40:00 +0200

    fix

--- hw8/task1.2.py
+++ hw8/task1.2.py
@@ -11,16 +11,13 @@ def index(lst, elem):  # returns integer or None
 
 def main():
     x = randint(3, 10)
-    elem = int(input())
+    elem = int(input('Введи число: '))
     random_lst = []
 
     for i in range(x):
         random_lst.append(randint(0, 1000))
 
-    print(index(random_lst, elem))
+    print(f'Индекс нашего числа {elem} в списке {random_lst} = {index(random_lst, elem)}')
 
 
 main()
-
-
-
