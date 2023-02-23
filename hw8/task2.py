b9ecaa6786d0fd807742255f2e8b518d6f010085
xsandersky <xsandersky@gmail.com>
2023-02-23 18:16:29 +0200

    tsk2 fix

--- hw8/task2.py
+++ hw8/task2.py
@@ -1,13 +1,32 @@
 def copydeep(obj):
-    lst = []
-    for i in range(len(obj)):
+
+    if isinstance(obj, list):
+        lst = []
+
+        for i in range(len(obj)):
+            
+            if isinstance(obj[i], list):
+                lst.append(copydeep(obj[i]))
+            elif isinstance(obj[i], tuple):
+                lst.append(copydeep(obj[i]))
+            else:
+                lst.append(obj[i])
+
+    elif isinstance(obj, tuple):
+        lst = ()
+
+        for i in range(len(obj)):
+            
+            if isinstance(obj[i], list):
+                lst.append(copydeep(obj[i]))
+            elif isinstance(obj[i], tuple):
+                lst.append(copydeep(obj[i]))
+            else:
+                lst.append(obj[i])
+    
+    else:
+        lst = obj
         
-        if isinstance(obj[i], list):
-            lst.append(copydeep(obj[i]))
-        elif isinstance(obj[i], tuple):
-            lst.append(copydeep(obj[i]))
-        else:
-            lst.append(obj[i])
 
     return lst
 
@@ -24,17 +43,3 @@ def main():
 
 
 main()
-
-'''
-    for i in range(len(obj)):
-        
-        if isinstance(obj[i], list):
-            lst.append(list(obj[i]))
-        elif isinstance(obj[i], tuple):
-            lst.append(tuple(obj[i]))
-        else:
-            lst.append(obj[i])
-
-    return lst
-'''
-
