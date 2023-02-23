72dd7aad864c468a40e2d49e3b05080397af32b0
xsandersky <xsandersky@gmail.com>
2023-02-23 18:23:50 +0200

    & C:/Users/office-pc/AppData/Local/Programs/Python/Python311/python.exe c:/Users/office-pc/Desktop/ithillel_basic_python_vitalii/hw8/task2.py

--- hw8/task2.py
+++ hw8/task2.py
@@ -2,9 +2,8 @@ def copydeep(obj):
 
     if isinstance(obj, list):
         lst = []
-
         for i in range(len(obj)):
-            
+
             if isinstance(obj[i], list):
                 lst.append(copydeep(obj[i]))
             elif isinstance(obj[i], tuple):
@@ -14,32 +13,25 @@ def copydeep(obj):
 
     elif isinstance(obj, tuple):
         lst = ()
-
         for i in range(len(obj)):
-            
+
             if isinstance(obj[i], list):
                 lst.append(copydeep(obj[i]))
             elif isinstance(obj[i], tuple):
                 lst.append(copydeep(obj[i]))
             else:
                 lst.append(obj[i])
-    
     else:
         lst = obj
         
-
     return lst
 
 
-
-
-
 def main():
-    lst1 = ['a', 1, 2.0, ['b']]
+    lst1 = [['b', ['a']]]#['a', 1, 2.0, ['b']]
     lst2 = copydeep(lst1)
-    lst1[3].append(0)
-    print(lst1, lst2)
-
+    #lst1[3].append(0)
+    print(lst1, lst2, sep='\n')
 
 
 main()
