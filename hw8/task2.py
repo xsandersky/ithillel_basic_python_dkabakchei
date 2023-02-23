93055a84a632217b289b08db89ae9d2d249f074d
xsandersky <65461492+xsandersky@users.noreply.github.com>
2023-02-23 19:27:07 +0300

    Update task2.py

--- hw8/task2.py
+++ hw8/task2.py
@@ -1,27 +1,40 @@
-def copydeep(obj):
-    lst = []
-    for i in range(len(obj)):
-        
-        if isinstance(obj[i], list):
-            lst.append(copydeep(obj[i]))
-        elif isinstance(obj[i], tuple):
-            lst.append(copydeep(obj[i]))
-        else:
-            lst.append(obj[i])
+def copydeep(obj)
+
+    if isinstance(obj, list):
+        lst = []
+        for i in range(len(obj)):
+
+            if isinstance(obj[i], list):
+                lst.append(copydeep(obj[i]))
+            elif isinstance(obj[i], tuple):
+                lst.append(copydeep(obj[i]))
+            else:
+                lst.append(obj[i])
+
+    elif isinstance(obj, tuple):
+        lst = ()
+        for i in range(len(obj)):
+
+            if isinstance(obj[i], list):
+                lst.append(copydeep(obj[i]))
+            elif isinstance(obj[i], tuple):
+                lst.append(copydeep(obj[i]))
+            else:
+                lst.append(obj[i])
+                
+    else:
+        lst = obj
 
     return lst
 
 
-
-
-
 def main():
+
     lst1 = ['a', 1, 2.0, ['b']]
     lst2 = copydeep(lst1)
     lst1[3].append(0)
-    print(lst1, lst2)
 
+    print(lst1, lst2, sep='\n')
 
 
 main()
-
