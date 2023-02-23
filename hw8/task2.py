e96bf618e6db5a31f2085233077ff08dc5c55a58
xsandersky <xsandersky@gmail.com>
2023-02-23 18:24:47 +0200

    fatality

--- hw8/task2.py
+++ hw8/task2.py
@@ -23,14 +23,16 @@ def copydeep(obj):
                 lst.append(obj[i])
     else:
         lst = obj
-        
+
     return lst
 
 
 def main():
+
     lst1 = [['b', ['a']]]#['a', 1, 2.0, ['b']]
     lst2 = copydeep(lst1)
     #lst1[3].append(0)
+    lst1[0][1].append('c')
     print(lst1, lst2, sep='\n')
 
 
