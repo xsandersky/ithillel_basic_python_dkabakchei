585bffbf0bed6e4a3e6b253131c1f81ce0fb88dc
xsandersky <65461492+xsandersky@users.noreply.github.com>
2023-02-23 19:39:44 +0300

    Update task2.py

--- hw8/task2.py
+++ hw8/task2.py
@@ -25,19 +25,6 @@ def copydeep(obj):
     else:
         lst = obj
 
-    elif isinstance(obj, tuple):
-        lst = ()
-        for i in range(len(obj)):
-
-            if isinstance(obj[i], list):
-                lst.append(copydeep(obj[i]))
-            elif isinstance(obj[i], tuple):
-                lst.append(copydeep(obj[i]))
-            else:
-                lst.append(obj[i])
-    else:
-        lst = obj
-
     return lst
 
 
