a1bb71935ecdfdcd4f0b2c95d946391881265e1b
xsandersky <xsandersky@gmail.com>
2023-02-23 18:42:18 +0200

    furfurfur

--- hw8/task2.py
+++ hw8/task2.py
@@ -25,30 +25,19 @@ def copydeep(obj):
     else:
         lst = obj
 
-    elif isinstance(obj, tuple):
-        lst = ()
-        for i in range(len(obj)):
-
-            if isinstance(obj[i], list):
-                lst.append(copydeep(obj[i]))
-            elif isinstance(obj[i], tuple):
-                lst.append(copydeep(obj[i]))
-            else:
-                lst.append(obj[i])
-    else:
-        lst = obj
-
     return lst
 
 
 def main():
 
     lst1 = ['a', 1, 2.0, ['b']]
-
-    lst1 = [['b', ['a']]]#['a', 1, 2.0, ['b']]
     lst2 = copydeep(lst1)
+    
     lst1[3].append(0)
 
+    a = 3
+    s=3
+    print(a, s)
     print(lst1, lst2, sep='\n')
 
 
