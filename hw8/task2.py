07f39bb69e26c515b030c0ce0aa3a465e93f95df
xsandersky <xsandersky@gmail.com>
2023-02-23 20:17:05 +0200

    finita

--- hw8/task2.py
+++ hw8/task2.py
@@ -1,48 +1,29 @@
 def copydeep(obj):
 
     if isinstance(obj, list):
-        lst = []
+        value = []
         for i in range(len(obj)):
+            value.append(copydeep(obj[i]))
 
-            if isinstance(obj[i], list):
-                lst.append(copydeep(obj[i]))
-            elif isinstance(obj[i], tuple):
-                lst.append(copydeep(obj[i]))
-            else:
-                lst.append(obj[i])
 
     elif isinstance(obj, tuple):
-        lst = ()
+        value = ()
         for i in range(len(obj)):
+            value += (copydeep(obj[i]),)
 
-            if isinstance(obj[i], list):
-                lst.append(copydeep(obj[i]))
-            elif isinstance(obj[i], tuple):
-                lst.append(copydeep(obj[i]))
-            else:
-                lst.append(obj[i])
-                
     else:
-        lst = obj
+        value = obj
 
-    return lst
+    return value
 
 
 def main():
 
-    lst1 = ['a', 1, 2.0, ['b']]
+    lst1 = ('a', 1, 2.0, ['b'])
     lst2 = copydeep(lst1)
-    lst3 = copydeep('hello')
-    lst4 = copydeep(123)
-    lst5 = ('a', 1, 2.0, ['b'])
-    lst6 = copydeep(lst5)
-
     lst1[3].append(0)
+    lst5 = copydeep(123)
+    print(lst1, lst2, lst5, sep='\n')
 
-    print(lst1, lst2, lst3, lst4, lst6, sep='\n')
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 585bffbf0bed6e4a3e6b253131c1f81ce0fb88dc
 main()
