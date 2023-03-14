9b6332e874b22d02b3ed4303741c132f5979a7d7
xsandersky <xsandersky@gmail.com>
2023-03-14 14:08:55 +0200

    add hw_13/task_2

---
+++ hw_13/task_2.py
@@ -0,0 +1,25 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Circle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.r = 5
+
+    def cheker_point(self, point):
+        if ((point.x - self.x)**2 + (point.y - self.y)**2) <= self.r ** 2:
+            return True
+        else:
+            return False
+
+
+
+if __name__=='__main__':
+    point = Point(2, 2)
+    circle = Circle(3, 3)
+
+    print(circle.cheker_point(point))
