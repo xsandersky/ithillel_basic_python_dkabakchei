33294c4c2a6916cf0b991166e41c29f1f76ff42e
xsandersky <xsandersky@gmail.com>
2023-03-14 14:12:00 +0200

    hw13 / fix issue

--- hw_13/task_2.py
+++ hw_13/task_2.py
@@ -8,7 +8,7 @@ class Circle:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.r = 1
+        self.r = 2
 
     def cheker_point(self, point):
         if (point.x - self.x)**2 + (point.y - self.y)**2 <= self.r ** 2:
@@ -17,9 +17,8 @@ class Circle:
             return False
 
 
-
 if __name__=='__main__':
     point = Point(2, 2)
-    circle = Circle(3, 3)
+    circle = Circle(4, 4)
 
     print(circle.cheker_point(point))
